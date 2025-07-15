e016c73941b698fc2249647ee63ffd738de86aa6
Hoang-heheboi <ngododo123@gmail.com>
2025-07-15 14:27:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ document.getElementById("button1").onclick = function() {
 audio.currentTime = 0;
     audio.play();
 }
+const audio2 = new Audio('Vineboom.mp3');
+document.getElementById("button2").onclick = function() {
+    audio2.currentTime = 0;
+    audio2.play();
+}
+const audio3 = new Audio('Rizz.mp3');
+document.getElementById("button3").onclick = function() {
+    audio3.currentTime = 0;
+    audio3.play();
+}
+const audio4 = new Audio('WompWomp.mp3');
+document.getElementById("button4").onclick = function() {
+    audio4.currentTime = 0;
+    audio4.play();
+}
+const audio5 = new Audio('sadViolin.mp3');
+document.getElementById("button5"). onclick = function() {
+    audio5.currentTime = 0;
+    audio5.play();
+}
+const audio6 = new Audio('Bruh.mp3');
+document.getElementById("button6").onclick = function() {
+    audio6.currentTime = 0;
+    audio6.play();
+}
+const audio7 = new Audio('AirHorn.mp3');
+document.getElementById("button7").onclick = function() {
+    audio7.currentTime = 0;
+    audio7.play();
+}
